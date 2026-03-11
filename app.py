7a08dd253da1029a4256fb442902100b6e976d19
gmgmgmt <ryouga0521.aki@gmail.com>
2026-03-12 07:33:13 +0900

    fix port

--- app.py
+++ app.py
@@ -41,5 +41,8 @@ def home():
         result = obfuscate(code)
     return render_template_string(HTML,result=result)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
